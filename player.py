ae316271367e663805ca9efdecb228d1d6e5d6d0
Vu Anh Khoi <vuanhkhoi0312@gmail.com>
2025-01-07 02:48:47 +0700

    Update player.py

---
+++ player.py
@@ -0,0 +1,69 @@
+from setting import *
+import pygame as pg
+import math
+
+class Player:
+    def __init__(self, game):
+        self.game = game
+        self.x, self.y = PLAYER_POS
+        self.angle = PLAYER_ANGLE
+    
+    def movement(self):
+        # Toán (Tại sao lập trình lại có toán chứ :v )
+        sin_a = math.sin(self.angle) # sin dùng cho đi thẳng
+        cos_a = math.cos(self.angle) # cos dùng cho đi ngang
+        dx, dy = 0, 0
+        speed = PLAYER_SPEED * self.game.delta_time
+        speed_sin = speed * sin_a
+        speed_cos = speed * cos_a
+
+        # Nút bấm
+        keys = pg.key.get_pressed()
+        if keys[pg.K_w]: # Nhấn W (K_w)
+            dx += speed_cos
+            dy += speed_sin
+
+        if keys[pg.K_s]: # Nhấn S (K_s)
+            dx += -speed_cos
+            dy += -speed_sin
+        
+        if keys[pg.K_a]: # Nhấn A (K_a)
+            dx += -speed_cos
+            dy += speed_sin
+
+        if keys[pg.K_d]: # Nhấn D (K_d)
+            dx += speed_cos
+            dy += -speed_sin
+
+        self.x += dx
+        self.y += dy
+        
+        if keys[pg.K_LEFT]:
+            self.angle -= PLAYER_ROT_SPEED * self.game.delta_time
+        if keys[pg.K_RIGHT]:
+            self.angle += PLAYER_ROT_SPEED * self.game.delta_time
+        self.angle %= math.tau # tau = 2*pi
+
+    def draw(self):
+        pg.draw.line(surface = self.game.screen, 
+                    color = 'yellow', 
+                    start_pos = (self.x * 100, self.y * 100),
+                    end_pos = (self.x * 100 + WIDTH * math.cos(self.angle),self.y * 100 + WIDTH * math.sin(self.angle)),
+                    width = 2)
+        
+        pg.draw.circle(surface = self.game.screen,
+                       color = 'green',
+                       center = (self.x * 100, self.y * 100),
+                       radius = 15)
+        
+    def update(self):
+        self.movement()
+    
+    @property
+    def pos(self):
+        return self.x, self.y
+    
+    @property
+    def map_pos(self):
+        return int(self.x), int(self.y)
+    
